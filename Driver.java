5e96f75e44fbdf350e159f21429d4d4d8e83b1a2
Person890 <44305879+Person890@users.noreply.github.com>
2021-11-13 17:19:30 -0800

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,23 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Driver {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cafe", "root", "skakalpes");
+
+            Statement statement = connection.createStatement();
+
+//            15 different functionalities
+            ResultSet resultSet = statement.executeQuery("select * from CUSTOMER");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("id"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
