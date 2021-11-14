e2523c054c6727d163c9e980425c135e3318e5a1
Person890 <44305879+Person890@users.noreply.github.com>
2021-11-13 19:37:58 -0800

    add new employee

--- Driver.java
+++ Driver.java
@@ -10,7 +10,9 @@ public class Driver {
 //             you must add your connection details here (username and password)
             );
 
-            Statement statement = connection.createStatement();
+          
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
 
 //            15 different functionalities
 //            view menu
@@ -23,8 +25,28 @@ public class Driver {
                         resultSet.getString("price"));
             }
 
-//
-//            resultSet = statement.executeQuery("insert values into  ");
+//          hire new employee
+            ResultSet resultSet2 = statement.executeQuery("select * from Employee");
+            resultSet2.moveToInsertRow();
+            resultSet2.updateInt("employeeID", 111);
+            resultSet2.updateString("name", "Patrick");
+            resultSet2.updateString("position", "barista");
+            resultSet2.updateDouble("salary", 22.00);
+
+            resultSet2.insertRow();
+
+            resultSet2.beforeFirst();
+
+            System.out.println("Displaying record after the Insert...");
+
+            while(resultSet2.next())
+            {
+                int employeeID = resultSet2.getInt("employeeID");
+                String name = resultSet2.getString("name");
+                String position = resultSet2.getString("position");
+                double salary = resultSet2.getDouble("salary");
+                System.out.println("ID: " + employeeID + " Name:" + name + " Position: " + position + " Salary: $" + salary);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
